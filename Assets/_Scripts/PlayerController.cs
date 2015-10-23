5806146de02a8e902c01ba46dee90561d105bb04
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2015-10-22 21:22:09 -0400

    Created Hero Prefab, added jump anim and sound

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -20,7 +20,7 @@ public class VelocityRange
     // PUBLIC INSTANCE VARIABLES
     public float vMin, vMax;
 
-    public VelocityRange(float vMin, float vMax)
+    public VelocityRange ( float vMin, float vMax )
     {
         this.vMin = vMin;
         this.vMax = vMax;
@@ -28,84 +28,132 @@ public class VelocityRange
 }
 
 // PLAYERCONTROLLER CLASS
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     // PUBLIC INSTANCE VARIABLES
     public float speed = 50f;
     public float jump = 500f;
 
-    public VelocityRange velocityRange = new VelocityRange( 300f, 1000f );
+    public VelocityRange velocityRange = new VelocityRange(300f, 1000f);
 
 
     // PRIVATE INSTANCE VARIABLES
     private AudioSource [] _audioSources;
     private AudioSource _coinSound;
+    private AudioSource _jumpSound;
     private Rigidbody2D _rigidBody2D;
     private Transform _transform;
-    private Animator _animator;     // use this later
+    private Animator _animator;
+    
 
-    private float _moving = 0f;
+    private float _movingValue = 0f;
+    private bool _isFacingRight = true;
+    private bool _isGrounded = true;
 
-	// Use this for initialization
-	void Start ()
-	{
-	    this._rigidBody2D = gameObject.GetComponent<Rigidbody2D>();
-	    this._transform = gameObject.GetComponent<Transform>();
-	    this._animator = gameObject.GetComponent<Animator>();
+    // Use this for initialization
+    void Start ()
+    {
+        this._rigidBody2D = gameObject.GetComponent<Rigidbody2D>();
+        this._transform = gameObject.GetComponent<Transform>();
+        this._animator = gameObject.GetComponent<Animator>();
 
         // Reference to audio source
         this._audioSources = gameObject.GetComponents<AudioSource>();
-	    this._coinSound = this._audioSources[0];
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate ()
-	{
-	    float forceX = 0f;
-	    float forceY = 0f;
+        this._coinSound = this._audioSources [0];
+        this._jumpSound = this._audioSources [1];
+    }
 
-	    float absVelX = Mathf.Abs( this._rigidBody2D.velocity.x );
-        float absVelY = Mathf.Abs( this._rigidBody2D.velocity.y );
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+        float forceX = 0f;
+        float forceY = 0f;
 
-	    this._moving = Input.GetAxis( "Horizontal" );    // value is between -1 and 1
+        float absVelX = Mathf.Abs(this._rigidBody2D.velocity.x);
+        float absVelY = Mathf.Abs(this._rigidBody2D.velocity.y);
 
-	    if (this._moving != 0)
-	    {
+        this._movingValue = Input.GetAxis("Horizontal");    // value is between -1 and 1
+
+        // Check if player is moving
+        if ( this._movingValue != 0 )
+        {
             // we're moving
-	        if (this._moving > 0)
-	        {
-                // set anim state
-	            this._animator.SetInteger( "AnimState", 1 );    // play walk clip
-
-	            // moving right
-	            if (absVelX < this.velocityRange.vMax)
-	            {
+            // set anim state
+            this._animator.SetInteger("AnimState", 1);    // play walk clip
+            if ( this._movingValue > 0 )     // moving right
+            {
+                this._isFacingRight = true;
+                this._flip();
+                if ( absVelX < this.velocityRange.vMax )
+                {
                     forceX = this.speed;
-	            }
-	        } else if (this._moving < 0)
-	        {
-                //moving left
+                }
+
+            }   else if ( this._movingValue < 0 )        //moving left
+            {
+                this._isFacingRight = false;
+                this._flip();
                 if ( absVelX < this.velocityRange.vMax )
                 {
                     forceX = -this.speed;
                 }
             }
-	    } else if (this._moving == 0)
-	    {
-            // we're idle
+        } else if ( this._movingValue == 0 ) // we're idle
+        {
             this._animator.SetInteger("AnimState", 0);  // play idle clip
         }
 
-        this._rigidBody2D.AddForce(new Vector2 (forceX, forceY));
-	    
+        // Check if player is jumping
+        if (Input.GetKey( "up" ) || Input.GetKey( KeyCode.W ))
+        {
+            // Check if the player is grounded
+            if (this._isGrounded)
+            {
+                // player is jumping
+                this._animator.SetInteger( "AnimState", 2 );       // play jump anim
+                if ( absVelY < this.velocityRange.vMax )
+                {
+                    forceY = this.jump;
+                    this._jumpSound.Play();
+                    this._isGrounded = false;
+                }
+            }
+            
+        }
 
-	}
+        // add force along X and Y axis depending on player input
+        this._rigidBody2D.AddForce(new Vector2(forceX, forceY));
+    }
 
+    // Play sound on collision with coin
     void OnCollisionEnter2D ( Collision2D otherCollider )
     {
         if ( otherCollider.gameObject.CompareTag("Coin") )
         {
             this._coinSound.Play();
+        }
+    }
+
+    // Set isGrounded true while touching ground
+    void OnCollisionStay2D(Collision2D otherCollider)
+    {
+        if (otherCollider.gameObject.CompareTag( "Platform" ))
+        {
+            this._isGrounded = true;
+        }
+    }
+
+    // PRIVATE METHODS
+    private void _flip()
+    {
+        if (this._isFacingRight )
+        {
+            this._transform.localScale = new Vector3( 1f, 1f, 1f );     // flip to face right ( normal )
+        }
+        else
+        {
+            this._transform.localScale = new Vector3( -1f, 1f, 1f );    // flip to face left
         }
     }
 }
